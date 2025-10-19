8012b99e4218a7b6913b1351df352a9a60fb9661
Boasfar27 <muhammadfarhan2747@gmail.com>
2025-10-20 01:19:54 +0700

    update

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function validateForm() {
     var password = document.getElementById("password").value;
 
     //validasi username dan password, username dan password bisa diganti sesuai keinginan
-    if (username === "adel" && password === "Pacar Kalian") {
+    if (username === "pacar kalian" && password === "pacar kalian") {
         Swal.fire({
             icon: "success",
             title: "Login berhasil!",
-            text: "Selamat datang Pacar Kaliankk",
+            text: "Selamat datang pacar kaliankk",
             showConfirmButton: false,
             timer: 1500,
         }).then(function () {
